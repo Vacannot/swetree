cb65287ee3e57e8efad77186ac71d5d7bad77eb9
Vacannot <simon9916@gmail.com>
2023-01-26 18:16:50 +0100

    getting started

---
+++ src/components/Stats.tsx
@@ -0,0 +1,7 @@
+// this will be the stats sign
+
+const Stats = () => {
+  <div> "yo" </div>;
+};
+
+export default Stats;
---
+++ src/components/Upcoming.tsx
@@ -0,0 +1,7 @@
+// this will be the upcoming words sign
+
+const Upcoming = () => {
+  return <div> "upcoming" </div>;
+};
+
+export default Upcoming;
