f6c9901ac9939bb28c80b007458e8beee5fa96a4
Vacannot <simon9916@gmail.com>
2023-01-27 03:19:24 +0100

    man this loading bar does not need to work like this

--- src/components/Inputword.tsx
+++ src/components/Inputword.tsx
@@ -1,8 +1,13 @@
+import React from "react";
+import styles from "@/styles/Inputword.module.scss";
+
 const InputWord = () => {
   return (
-    <div>
-      <div>hej</div>
-      <div></div>
+    <div className={styles.center}>
+      <form action="" className={styles.form}>
+        <input type="text" id="input" className={styles.inputword} />
+      </form>
+      <div>This is where text shows up when you answer!</div>
     </div>
   );
 };
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,6 +1,5 @@
 import styles from "@/styles/Stats.module.scss";
 import React from "react";
-
 import { useState } from "react";
 
 const Stats = () => {
@@ -13,7 +12,17 @@ const Stats = () => {
   return (
     <div className={styles.sign}>
       <div className={styles.header}>Progress</div>
-      <div className={styles.loading}>LOADINGBAR</div>
+      <div className={styles.loading}>
+        <div className={styles.loadingbar}>
+          <div className={styles.loadingsectionshorter} id="loading1"></div>
+          <div className={styles.loadingsectionblue} id="loading2"></div>
+          <div className={styles.loadingsection} id="loading3"></div>
+          <div className={styles.loadingsectionblue} id="loading4"></div>
+          <div className={styles.loadingsection} id="loading5"></div>
+          <div className={styles.loadingsectionblue} id="loading6"></div>
+          <div className={styles.loadingsectionshort} id="loading7"></div>
+        </div>
+      </div>
       <div className={styles.title}>Level:</div>
       <div className={styles.value}>{level}</div>
       <div className={styles.title}>Water per word:</div>
