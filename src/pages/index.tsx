d3cc377ab1d7027c3950911736d270c9c330db86
Vacannot <simon9916@gmail.com>
2023-03-31 12:29:04 +0200

    changed naming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
-import SweTree from "./playground";
+import SweTree from "./SweTree";
 
 export default function Home() {
   return (
